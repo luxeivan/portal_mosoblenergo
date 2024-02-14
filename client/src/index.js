f3df47bd0be66cc93cbf4450f6c936958bda2fb9
Stanislav <yanut995@gmail.com>
2024-02-14 15:57:20 +0300

    Fix header

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app/App';
-import reportWebVitals from './app/reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./app/App";
+import reportWebVitals from "./app/reportWebVitals";
+import { ConfigProvider } from "antd";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <ConfigProvider
+    theme={{
+      components: {
+        Layout: {
+          headerBg: "inherit",
+        },
+      },
+    }}
+  >
     <App />
+  </ConfigProvider>
 );
 
 reportWebVitals();

8bfb613c57621f83cacca5df68fa156c6e688542
Stanislav <yanut995@gmail.com>
2024-02-15 15:43:45 +0300

    Add Tabs in Auth

--- client/src/pages/AuthPage/AuthPage.js
+++ client/src/pages/AuthPage/AuthPage.js
@@ -1,7 +1,12 @@
+
 import React from "react";
 import styles from "./AuthPage.module.css";
 import { Helmet } from "react-helmet";
 import FormLogin from "../../features/FormLogin/FormLogin";
+import FormRegistration from "../../features/FormRegistration/FormRegistration";
+import { Tabs } from "antd";
+
+const { TabPane } = Tabs;
 
 export default function AuthPage() {
   return (
@@ -9,7 +14,15 @@ export default function AuthPage() {
       <Helmet>
         <title>Авторизация</title>
       </Helmet>
-      <FormLogin />
+      <Tabs defaultActiveKey="1" centered>
+        <TabPane tab="Войти" key="1">
+          <FormLogin />
+        </TabPane>
+        <TabPane tab="Регистрация" key="2">
+          <FormRegistration />
+        </TabPane>
+      </Tabs>
     </div>
   );
 }
+

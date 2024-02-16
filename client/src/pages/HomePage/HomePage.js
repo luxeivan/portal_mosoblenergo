390e8d6f80399e42b7ac94896151c6a33b4cc2e9
luxeivan <luxeivan@gmail.com>
2024-02-16 08:19:23 +0300

    commit

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-import styles from './HomePage.module.css'
-
-=======
-import React from "react";
+import React,{useState} from "react";
 import styles from "./HomePage.module.css";
 import { Helmet } from "react-helmet";
->>>>>>> 43e6627d187e358a02bcab7e405e0ba54a0ab3fa
 
 export default function HomePage() {
   const [deg, setDeg] = useState(0)
@@ -15,15 +9,6 @@ export default function HomePage() {
     // setDeg(prev => ++prev)
   }, 1000)
   return (
-<<<<<<< HEAD
-    <div className={styles.homePage} style={{ backgroundImage: `linear-gradient(${deg}deg, rgba(0, 97, 175, 1) 0%, rgba(0, 143, 213, 1) 50%, rgba(0, 97, 175, 1) 100%)` }}>
-      <p>
-        Проверка
-      </p>
-      {/* <button className={styles.button}>Кнопка</button> */}
-    </div>
-  )
-=======
     <div className={styles.homePage}>
       <Helmet>
         <title>Главная страничка</title>
@@ -31,5 +16,4 @@ export default function HomePage() {
       <div></div>
     </div>
   );
->>>>>>> 43e6627d187e358a02bcab7e405e0ba54a0ab3fa
 }

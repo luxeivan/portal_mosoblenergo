0ba65ff8906a696de51435d6f744b5efd7a1e3c0
Stanislav <yanut995@gmail.com>
2024-02-16 10:06:13 +0300

    Mini fix

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,13 +1,7 @@
-import React,{useState} from "react";
 import styles from "./HomePage.module.css";
 import { Helmet } from "react-helmet";
 
 export default function HomePage() {
-  const [deg, setDeg] = useState(0)
-  setInterval(() => {
-    // if (deg === 360) return setDeg(prev => 0)
-    // setDeg(prev => ++prev)
-  }, 1000)
   return (
     <div className={styles.homePage}>
       <Helmet>
